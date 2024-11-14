84f106d91ad65d56928634d0acde4bdf43e09391
osunapc <osunapc@gmail.com>
2024-11-14 18:30:15 -0400

    integracion de paginas mision vision objetivos y organigrama

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'INPRADEM';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Proteccion Civil Merida';
+export const SITE_DESCRIPTION = "Una institución estratégica para la nación y de reconocida proyección internacional, donde se impulsa un polo importante de la seguridad ciudadana basada en la prevención y atención, por la salvaguarda y mejor calidad de vida, el ambiente y los bienes, ante amenazas naturales y antrópicas, contribuyendo con la sostenibilidad del desarrollo del País. ";
