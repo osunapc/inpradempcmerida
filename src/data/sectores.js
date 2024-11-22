7214f80f81b6eb974b6ab2fcd98a666bb18df9dc
osunapc <osunapc@gmail.com>
2024-11-22 16:20:08 -0400

    avance con los sectores

---
+++ src/data/sectores.js
@@ -0,0 +1,95 @@
+export const sectores = [
+  {
+    id: 1,
+    nombre: "SUBCENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL ARICAGUA",
+    coordinador: "APC LICDO Jan Carlos Sosa",
+    direccion: "Sector, El Porvenir, vía principal al complejo ferial y deportivo Aricagua.",
+    municipio: "Aricagua",
+    parroquia: "Aricagua",
+    contacto: {
+      telefono: "04268269937",
+      email: "pcmeridasccpcaricagua@gmail.com"
+    },
+    personal: {
+      total: 15,
+      operativo: 13,
+      administrativo: 0
+    },
+    vehiculos: [
+      {
+        modelo: "GACELA 06 ZUZUKI DR 200",
+        tipo: "MOTO",
+        placa: "AA1S23A",
+        color: "BLANCO"
+      }
+    ],
+    limites: {
+      norte: "Municipio Libertador del Estado Mérida",
+      sur: "Estado Barinas",
+      este: "Estado Barinas",
+      oeste: "Municipio Arzobispo Chacón del Estado Mérida"
+    },
+    coordenadas: {
+      latitud: "8.22469",
+      longitud: "-71.1370"
+    }
+  },
+  {
+    id:2,
+    nombre: "CENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL «PANAMERICANA SUR»",
+    direccion: "Urb. Las Acacias II Calle principal al lado del Parque Francisco de Miranda",
+    municipio: "Alberto Adriani",
+    parroquia: "Rómulo Betancourt",
+    contacto: {
+      telefono: "0412-6573445",
+      email: "pcmeridave.sectorpanamericana@Gmail.com"
+    },
+    personal: {
+      total: 37,
+      operativo: 32,
+      administrativo: 5
+    },
+    vehiculos: [
+      {
+        modelo: "FOTON L-STM-BJ10412-5T",
+        tipo: "Ambulancia",
+        placa: "A55AO8R",
+        color: "BLANCO"
+      },
+      {
+        modelo: "JAC HFC-1040M4",
+        tipo: "Ambulancia",
+        placa: "AL5DC7G",
+        color: "ROJO"
+      },
+      {
+        modelo: "JAC J52",
+        placa: "AB164JH",
+        color: "BLANCO"
+      },
+      {
+        modelo: "CHEVROLET LUV-DIMAX",
+        tipo: "RESCATE",
+        placa: "A35AB1A",
+        color: "BLANCO"
+      },
+      {
+        modelo: "SUZUKI GN-125",
+        tipo: "MOTO",
+        placa: "AB5D22A",
+        color: "NEGRA"
+      }
+    ],
+    limites: {
+      norte: "Municipios Colón, Francisco Javier Pulgar (ESTADO ZULIA)",
+      sur: "Municipios Zea, Sucre y Antonio Pinto Salinas (ESTADO MÉRIDA)",
+      este: "Municipios Sucre, Andrés Bello, Obispo Ramos de Lora (ESTADO MÉRIDA)",
+      oeste: "Municipio Samuel Darío Maldonado (ESTADO TÁCHIRA) Municipio Zea"
+    },
+    coordenadas: {
+      latitud: "8°29' 30\" y 8°46' 20\"",
+      longitud: "71°0' 33\" y 71°54' 40\""
+    }
+  }
+];
+
