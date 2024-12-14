8b9f56439e18a8ef31ba300cd7ea8afd99154116
osunapc <osunapc@gmail.com>
2024-12-14 11:42:19 -0400

    se agregaron los sectores

--- src/data/sectores.js
+++ src/data/sectores.js
@@ -1,95 +1,469 @@
 export const sectores = [
-  {
-    id: 1,
-    nombre: "SUBCENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL ARICAGUA",
-    coordinador: "APC LICDO Jan Carlos Sosa",
-    direccion: "Sector, El Porvenir, vía principal al complejo ferial y deportivo Aricagua.",
-    municipio: "Aricagua",
-    parroquia: "Aricagua",
-    contacto: {
-      telefono: "04268269937",
-      email: "pcmeridasccpcaricagua@gmail.com"
-    },
-    personal: {
-      total: 15,
-      operativo: 13,
-      administrativo: 0
-    },
-    vehiculos: [
-      {
-        modelo: "GACELA 06 ZUZUKI DR 200",
-        tipo: "MOTO",
-        placa: "AA1S23A",
-        color: "BLANCO"
-      }
-    ],
-    limites: {
-      norte: "Municipio Libertador del Estado Mérida",
-      sur: "Estado Barinas",
-      este: "Estado Barinas",
-      oeste: "Municipio Arzobispo Chacón del Estado Mérida"
-    },
-    coordenadas: {
-      latitud: "8.22469",
-      longitud: "-71.1370"
-    }
-  },
-  {
-    id:2,
-    nombre: "CENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL «PANAMERICANA SUR»",
-    direccion: "Urb. Las Acacias II Calle principal al lado del Parque Francisco de Miranda",
-    municipio: "Alberto Adriani",
-    parroquia: "Rómulo Betancourt",
-    contacto: {
-      telefono: "0412-6573445",
-      email: "pcmeridave.sectorpanamericana@Gmail.com"
-    },
-    personal: {
-      total: 37,
-      operativo: 32,
-      administrativo: 5
-    },
-    vehiculos: [
-      {
-        modelo: "FOTON L-STM-BJ10412-5T",
-        tipo: "Ambulancia",
-        placa: "A55AO8R",
-        color: "BLANCO"
-      },
-      {
-        modelo: "JAC HFC-1040M4",
-        tipo: "Ambulancia",
-        placa: "AL5DC7G",
-        color: "ROJO"
-      },
-      {
-        modelo: "JAC J52",
-        placa: "AB164JH",
-        color: "BLANCO"
-      },
-      {
-        modelo: "CHEVROLET LUV-DIMAX",
-        tipo: "RESCATE",
-        placa: "A35AB1A",
-        color: "BLANCO"
-      },
-      {
-        modelo: "SUZUKI GN-125",
-        tipo: "MOTO",
-        placa: "AB5D22A",
-        color: "NEGRA"
-      }
-    ],
-    limites: {
-      norte: "Municipios Colón, Francisco Javier Pulgar (ESTADO ZULIA)",
-      sur: "Municipios Zea, Sucre y Antonio Pinto Salinas (ESTADO MÉRIDA)",
-      este: "Municipios Sucre, Andrés Bello, Obispo Ramos de Lora (ESTADO MÉRIDA)",
-      oeste: "Municipio Samuel Darío Maldonado (ESTADO TÁCHIRA) Municipio Zea"
-    },
-    coordenadas: {
-      latitud: "8°29' 30\" y 8°46' 20\"",
-      longitud: "71°0' 33\" y 71°54' 40\""
-    }
-  }
+	{
+		id: 1,
+		img: "/sectores/aricagua.avif",
+		nombre: "SUBCENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL ARICAGUA",
+		coordinador: "APC LICDO Jan Carlos Sosa",
+		direccion:
+			"Sector, El Porvenir, vía principal al complejo ferial y deportivo Aricagua.",
+		municipio: "Aricagua",
+		parroquia: "Aricagua",
+		contacto: {
+			telefono: "04268269937",
+			email: "pcmeridasccpcaricagua@gmail.com",
+		},
+		personal: {
+			total: 15,
+			operativo: 13,
+			administrativo: 0,
+		},
+		vehiculos: [
+			{
+				modelo: "GACELA 06 ZUZUKI DR 200",
+				tipo: "MOTO",
+				placa: "AA1S23A",
+				color: "BLANCO",
+			},
+		],
+		limites: {
+			norte: "Municipio Libertador del Estado Mérida",
+			sur: "Estado Barinas",
+			este: "Estado Barinas",
+			oeste: "Municipio Arzobispo Chacón del Estado Mérida",
+		},
+		coordenadas: {
+			latitud: "8.22469",
+			longitud: "-71.1370",
+		},
+	},
+	{
+		id: 2,
+		img: "/sectores/panamericanaSur.avif",
+		nombre: "CENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL «PANAMERICANA SUR»",
+		coordinador: "CGPC Licda. Carmen Castañeda",
+		direccion:
+			"Urb. Las Acacias II Calle principal al lado del Parque Francisco de Miranda",
+		municipio: "Alberto Adriani",
+		parroquia: "Rómulo Betancourt",
+		contacto: {
+			telefono: "0412-6573445",
+			email: "pcmeridave.sectorpanamericana@Gmail.com",
+		},
+		personal: {
+			total: 37,
+			operativo: 32,
+			administrativo: 5,
+		},
+		vehiculos: [
+			{
+				modelo: "FOTON L-STM-BJ10412-5T",
+				tipo: "Ambulancia",
+				placa: "A55AO8R",
+				color: "BLANCO",
+			},
+			{
+				modelo: "JAC HFC-1040M4",
+				tipo: "Ambulancia",
+				placa: "AL5DC7G",
+				color: "ROJO",
+			},
+			{
+				modelo: "JAC J52",
+				tipo: "",
+				placa: "AB164JH",
+				color: "BLANCO",
+			},
+			{
+				modelo: "CHEVROLET LUV-DIMAX",
+				tipo: "RESCATE",
+				placa: "A35AB1A",
+				color: "BLANCO",
+			},
+			{
+				modelo: "SUZUKI GN-125",
+				tipo: "MOTO",
+				placa: "AB5D22A",
+				color: "NEGRA",
+			},
+		],
+		limites: {
+			norte: "Municipios Colón, Francisco Javier Pulgar (ESTADO ZULIA)",
+			sur: "Municipios Zea, Sucre y Antonio Pinto Salinas (ESTADO MÉRIDA)",
+			este: "Municipios Sucre, Andrés Bello, Obispo Ramos de Lora (ESTADO MÉRIDA)",
+			oeste: "Municipio Samuel Darío Maldonado (ESTADO TÁCHIRA) Municipio Zea",
+		},
+		coordenadas: {
+			latitud: "8°29' 30\" y 8°46' 20\"",
+			longitud: "71°0' 33\" y 71°54' 40\"",
+		},
+	},
+	{
+		id: 3,
+		img: "/sectores/panamericanaNorte.avif",
+		nombre: "CENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL «PANAMERICANA NORTE»",
+		coordinador: "OSPC III Alexander Rondon",
+		direccion: "El Pinar Sector los Pinos al lado de la Chocolatera Andinito",
+		municipio: "Caracciolo parra y olmedo",
+		parroquia: "Florencio Ramirez",
+		contacto: {
+			telefono: "0424-7097391",
+			email: "ccpcsurdellago5@Gmail.com",
+		},
+		personal: {
+			total: 18,
+			operativo: 14,
+			administrativo: 4,
+		},
+		vehiculos: [
+			{
+				modelo: "Horse Empire",
+				tipo: "Motorizada",
+				placa: "AA1520A",
+				color: "Negro",
+			},
+		],
+		limites: {
+			norte: "Parroquia Tucani del Municipio Caracciolo Parra Olmedo",
+			sur: "Parroquia Heras del Municipio Sucre (Zulia)",
+			este: "Parroquia San Rafael de Alcazar del Municipio Obispo Ramos de Lora",
+			oeste: "Parroquia Páramo del Municipio La Culata",
+		},
+		coordenadas: {
+			latitud: "8.96918",
+			longitud: "-71.2742",
+		},
+	},
+	{
+		id: 4,
+		img: "/sectores/canagua.avif",
+		nombre: "CENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL «PUEBLOS DEL SUR»",
+		coordinador: "OPC III Sergio moron",
+		direccion: "kilometro 1 via el valle, avenida hugo chavez frias",
+		municipio: "arzobispo chacón",
+		parroquia: "Canagua",
+		contacto: {
+			telefono: "0275-8681167",
+			email: "ccpcpueblosdelsur.22@gmail.com",
+		},
+		personal: {
+			total: 36,
+			operativo: 33,
+			administrativo: 3,
+		},
+		vehiculos: [
+			{
+				modelo: "TOYOTA 4.500",
+				tipo: "CHASI LARGA",
+				placa: "LAV03F",
+				color: "BLANCO CON FRANJAS ROJO Y AZUL",
+			},
+			{
+				modelo: "TOYOTA AMBULANCIA",
+				tipo: "CHASIS LARGO",
+				placa: "DBX10K",
+				color: "BLANCO CON FRANJAS ROJO Y AZUL",
+			},
+			{
+				modelo: "MOTO DR650",
+				tipo: "ENDURO",
+				placa: "AI0E41G",
+				color: "BLANCO",
+			},
+		],
+		limites: {
+			norte: "Aldea las Aguadas",
+			sur: "Parroquia Guaimaral",
+			este: "Cerro las Angustias",
+			oeste: "Cerro palma sola",
+		},
+		coordenadas: {
+			latitud: "8.09019",
+			longitud: "-71.4299",
+		},
+	},
+	{
+		id: 5,
+		img: "/sectores/mucuchies.avif",
+		nombre: "CENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL PARAMO",
+		coordinador: "CPC I LCDO. FRANCISCO SUESCUN",
+		direccion:
+			"AV. Carabobo con Calle Espari, Edificio Proteccion Civil Mucuchíes Estado Mérida",
+		municipio: "Rangel",
+		parroquia: "mucuchies",
+		contacto: {
+			telefono: "0274-8720215",
+			email: "pcmeridave.sectorparamo@Gmail.com",
+		},
+		personal: {
+			total: 22,
+			operativo: 19,
+			administrativo: 3,
+		},
+		vehiculos: [
+			{
+				modelo: "DR 200",
+				tipo: "Moto - gacela 7",
+				placa: "AE1E09A",
+				color: "Roja",
+			},
+			{
+				modelo: "DR 200",
+				tipo: "Moto – gacela 2",
+				placa: "AE1E16A",
+				color: "Blanca",
+			},
+			{
+				modelo: "SUNRAY JAC",
+				tipo: "Ambulancia Luz Caraballo",
+				placa: "AC573EL",
+				color: "Blanco",
+			},
+		],
+		limites: {
+			norte: "",
+			sur: "",
+			este: "",
+			oeste: "",
+		},
+		coordenadas: {
+			latitud: "",
+			longitud: "",
+		},
+	},
+	{
+		id: 6,
+		img: "/sectores/metropolitano.avif",
+		nombre: "CENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL METROPOLITANO",
+		coordinador: "OSPC III LCDA DIANA MEIJON",
+		direccion:
+			"Avenida Bolívar de La Parroquia, funciona Registro Civil, Prefectura e IPOSTEL",
+		municipio: "Libertador",
+		parroquia: "Juan Rodriguez Suarez",
+		contacto: {
+			telefono: "0424-741.03.59 / 0426-626.49.96",
+			email: "pcmeridave.ccpcmetropolitano1@gmail.com",
+		},
+		personal: {
+			total: 32,
+			operativo: 26,
+			administrativo: 5,
+		},
+		vehiculos: [
+			{
+				modelo: "TOYOTA LAND CRUISER MACHITO",
+				tipo: "",
+				placa: "SIN PLACA",
+				color: "BLANCO",
+			},
+			{
+				modelo: "SUZUKI DR650",
+				tipo: "MOTO",
+				placa: "SIN PLAZA",
+				color: "BLANCO – ROJO",
+			},
+		],
+		limites: {
+			norte: "Parroquia Osuna Rodríguez del Municipio Libertador",
+			sur: "Parroquia Jacinto Plaza del Municipio Libertador",
+			este: "Parroquia Domingo Peña del Municipio Libertador",
+			oeste: "Parroquia Montalbán del Municipio Campo Elías",
+		},
+		coordenadas: {
+			latitud: "8.59833",
+			longitud: "-71.145",
+		},
+	},
+	{
+		id: 7,
+		img: "/sectores/anis.avif",
+		nombre: "SUB CENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL EL ANIS",
+		coordinador: "OSPC I OSCAR PARRA",
+		direccion:
+			"Carretera la Variante Troncal 007 , Sector Llano La Honda el Anís ,entrada Chiguara",
+		municipio: "SUCRE",
+		parroquia: "Chiguara",
+		contacto: {
+			telefono: "0412-2997253 - 0426-1780068",
+			email: "pcmeridave.caeelanis@gmail.com",
+		},
+		personal: {
+			total: 32,
+			operativo: 27,
+			administrativo: 5,
+		},
+		vehiculos: [
+			{
+				modelo: "JAC Sunray",
+				tipo: "Ambulancia",
+				placa: "AC771CL",
+				color: "Blanca",
+			},
+			{
+				modelo: "Empire Horse",
+				tipo: "Moto",
+				placa: "S/P",
+				color: "Negra",
+			},
+		],
+		limites: {
+			norte: "Parroquia Chiguará del Municipio Sucre",
+			sur: "Parroquia Estanques del Municipio Sucre",
+			este: "Parroquia Estanques del Municipio Sucre",
+			oeste: "Parroquia Lagunillas del Municipio Sucre",
+		},
+		coordenadas: {
+			latitud: "8.50278",
+			longitud: "-71.3917",
+		},
+	},
+	{
+		id: 8,
+		img: "/sectores/santaElena.avif",
+		nombre: "SUB CENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL SANTA ELENA",
+		coordinador: "APC SIMÓN ALVAREZ",
+		direccion: "Vereda 1",
+		municipio: "Obispo Ramos de Lora",
+		parroquia: "Santa Elena de Arenales",
+		contacto: {
+			telefono: "0424 7114594",
+			email: "Pcmeridave.caesantaelena@Gmail.com",
+		},
+		personal: {
+			total: 42,
+			operativo: 40,
+			administrativo: 2,
+		},
+		vehiculos: [
+			{
+				modelo: "IVECO POWER EURO II",
+				tipo: "AMBULANCIA",
+				placa: "A07CM5D",
+				color: "BLANCO",
+			},
+			{
+				modelo: "TOYOTA HIACE",
+				tipo: "AMBULANCIA",
+				placa: "2700AC",
+				color: "BLANCO",
+			},
+			{
+				modelo: "CHEVROLET LUV-DIMAX",
+				tipo: "PICK-UP",
+				placa: "A96AY8V",
+				color: "BLANCO",
+			},
+			{
+				modelo: "EMPIRE KEEWAY HORSE",
+				tipo: "MOTO GACELA-28",
+				placa: "",
+				color: "NEGRO",
+			},
+		],
+		limites: {
+			norte: "Estado Zulia",
+			sur: "Parroquia La Azulita del Municipio Andrés Bello",
+			este: "Parroquia Florencio Ramírez del Municipio Caracciolo Parra Olmedo",
+			oeste: "Parroquia Héctor Amable Mora del Municipio Alberto Adriani",
+		},
+		coordenadas: {
+			latitud: "8.822009",
+			longitud: "-71.466637",
+		},
+	},
+	{
+		id: 9,
+		img: "/sectores/murachi.avif",
+		nombre: "FUERZA DE TAREA HUMANITARIA MURACHI",
+		coordinador: "OSPC III Marianelly moreno",
+		direccion: "Av los proceres, al lado de la estación de servicio buganvilas",
+		municipio: "libertador",
+		parroquia: "Caracciolo parra",
+		contacto: {
+			telefono: "",
+			email: "",
+		},
+		personal: {
+			total: 69,
+			operativo: 64,
+			administrativo: 5,
+		},
+		vehiculos: [
+			{
+				modelo: "Sunray JAC",
+				tipo: "Ambulancia Heroína María Simona",
+				placa: "AC773CL",
+				color: "Blanco",
+			},
+			{
+				modelo: "Toyota Land Cruiser",
+				tipo: "Rescate",
+				placa: "Pantera 05",
+				color: "Blanco",
+			},
+			{
+				modelo: "IVECO Pegaso",
+				tipo: "Multipropósito Magirus",
+				placa: "01",
+				color: "Blanco",
+			},
+		],
+		limites: {
+			norte: "",
+			sur: "",
+			este: "",
+			oeste: "",
+		},
+		coordenadas: {
+			latitud: "",
+			longitud: "",
+		},
+	},
+	{
+		id: 10,
+		img: "/sectores/mocoties.avif",
+		nombre: "CENTRO DE COORDINACIÓN DE PROTECCIÓN CIVIL MOCOTIES",
+		coordinador: "ING VIVIANA ESCALANTE",
+		direccion: "Funda Agrícola LA Jabonera",
+		municipio: "TOVAR",
+		parroquia: "TOVAR",
+		contacto: {
+			telefono: "",
+			email: "Pcmeridave.sectormocoties@gmail.com",
+		},
+		personal: {
+			total: 30,
+			operativo: 30,
+			administrativo: 1,
+		},
+		vehiculos: [
+			{
+				modelo: "SUNRAY",
+				tipo: "AMBULANCIA",
+				placa: "AC772CL",
+				color: "BLANCO",
+			},
+			{
+				modelo: "FORD FORTALEZA 150",
+				tipo: "CAMIONETA – ADMINISTRATIVO",
+				placa: "87WABD",
+				color: "BLANCO",
+			},
+			{
+				modelo: "HORSE",
+				tipo: "MOTO",
+				placa: "AE7L32U",
+				color: "AZUL",
+			},
+		],
+		limites: {
+			norte: "Parroquia Tovar Sector San Pedro",
+			sur: "Parroquia Tovar Sector Loma La Virgen",
+			este: "Parroquia Tovar Sector El Peñon",
+			oeste: "Parroquia Tovar Avenida Cipriano Castro",
+		},
+		coordenadas: {
+			latitud: "8°14′37″ y 8°26′30″",
+			longitud: "71°39′10″ y 71°48′24″",
+		},
+	},
 ];
-
