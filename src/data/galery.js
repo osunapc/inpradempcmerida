c154f4c56a1b24099d952ba10e9177e1b19125ed
osunapc <osunapc@gmail.com>
2025-01-30 11:07:56 -0400

    se agregó la página de galería y se actualizaron las dependencias

---
+++ src/data/galery.js
@@ -0,0 +1,20 @@
+export const galeria = [
+	{
+		img: "/galeria/pcmerida1.avif",
+		alt: "fucionarios de educion pamerida",
+	},
+	{
+		img: "/galeria/pcmerida2.avif",
+		alt: "fucionarios de educion pamerida",
+	},
+	{ img: "/galeria/pcmerida3.avif", alt: "fucionarios de educion pamerida" },
+	{ img: "/galeria/pcmerida4.avif", alt: "fucionarios de educion pamerida" },
+	{ img: "/galeria/pcmerida5.avif", alt: "fucionarios de educion pamerida" },
+	{ img: "/galeria/pcmerida6.avif", alt: "fucionarios de educion pamerida" },
+	{ img: "/galeria/pcmerida7.avif", alt: "fucionarios de educion pamerida" },
+	{ img: "/galeria/pcmerida8.avif", alt: "fucionarios de educion pamerida" },
+	{ img: "/galeria/pcmerida9.avif", alt: "fucionarios de educion pamerida" },
+	{ img: "/galeria/pcmerida10.avif", alt: "fucionarios de educion pamerida" },
+	{ img: "/galeria/pcmerida11.avif", alt: "fucionarios de educion pamerida" },
+	{ img: "/galeria/pcmerida12.avif", alt: "fucionarios de educion pamerida" },
+];
